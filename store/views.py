ddf9e8db5b26aabc269aeebfc3159edc916cd9db
Masaki Otsuka <motsuka@soka.edu>
2020-09-22 11:59:19 +0900

    render product

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all() # retrieve all objects from a table
+    context = {"products": products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
